3a459681ed09a32a82c632328bc1a67c72e80af9
Dylan Maimaiti <dmaimait@uoguelph.ca>
2023-01-11 14:26:22 -0500

    Created user database schema

---
+++ server/models/user.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+//collection is a group of documents
+//documents have the actual data
+//creating the document schema
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    displayName: {
+        type: String,
+        required: true
+    },
+    stats: {
+        type: Object,
+        required: true
+    }
+}, {timestamps: true});
+
+//making a model
+//it pluralises blog so itll look for blogs
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
